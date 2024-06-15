9db01f3c97621bfbbd0398f6de32c061a3dba859
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-15 10:15:40 -0700

    Add function and test to Animal
    - Add did_flee_succeed funct base on flee_success_rate, bool
    - Add test to did_flee_succeed funct, same test as hunt succeed
    - Tested function

--- animal.py
+++ animal.py
@@ -16,6 +16,11 @@ class Animal:
             return True
         return False
 
+    def did_flee_succeed(self) -> bool:
+        if self.roll() < self.flee_success_rate:
+            return True
+        return False
+
 
 def test_did_hunt_suceed(hunt_success_rate, test_num=1000):
     anim = Animal()
@@ -29,8 +34,25 @@ def test_did_hunt_suceed(hunt_success_rate, test_num=1000):
         if (hunt_sucess):
             success += 1
 
-    print(f'Success rate: {success / test_case}')
+    print(f'Hunt success rate: {success / test_case}')
+
+
+def test_did_flee_suceed(flee_success_rate, test_num=1000):
+    anim = Animal()
+    anim.flee_success_rate = flee_success_rate
+    test_case = test_num
+    success = 0
+    for i in range(test_case):
+
+        flee_success = anim.did_flee_succeed()
+        # print(hunt_sucess)
+        if (flee_success):
+            success += 1
+
+    print(f'Flee success rate: {success / test_case}')
 
 
 test_did_hunt_suceed(0.8, 1000)
 test_did_hunt_suceed(0.1, 1000)
+test_did_flee_suceed(0.3, 1000)
+test_did_flee_suceed(1, 1000)
