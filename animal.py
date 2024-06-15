88705f70253cf05b66998d440fadc823ebcc58fc
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-15 10:24:46 -0700

    Modified function name, Add new function
    - Modified did_hunt_succeed to is_hunt_successful
    - Modified did_flee_succeed to is_flee_successful
    - Add has_starve function to check if Animal has starved
    - Add test function for has_starve

--- animal.py
+++ animal.py
@@ -11,25 +11,30 @@ class Animal:
     def roll(self):
         return random.random()
 
-    def did_hunt_succeed(self) -> bool:
+    def is_hunt_successful(self) -> bool:
         if self.roll() < self.hunt_success_rate:
             return True
         return False
 
-    def did_flee_succeed(self) -> bool:
+    def is_flee_successful(self) -> bool:
         if self.roll() < self.flee_success_rate:
             return True
         return False
 
+    def has_starved(self) -> bool:
+        if self.starvation_time < 0:
+            return True
+        return False
 
-def test_did_hunt_suceed(hunt_success_rate, test_num=1000):
+
+def test_is_hunt_successful(hunt_success_rate, test_num=1000):
     anim = Animal()
     anim.hunt_success_rate = hunt_success_rate
     test_case = test_num
     success = 0
     for i in range(test_case):
 
-        hunt_sucess = anim.did_hunt_succeed()
+        hunt_sucess = anim.is_hunt_successful()
         # print(hunt_sucess)
         if (hunt_sucess):
             success += 1
@@ -37,14 +42,14 @@ def test_did_hunt_suceed(hunt_success_rate, test_num=1000):
     print(f'Hunt success rate: {success / test_case}')
 
 
-def test_did_flee_suceed(flee_success_rate, test_num=1000):
+def test_is_flee_successful(flee_success_rate, test_num=1000):
     anim = Animal()
     anim.flee_success_rate = flee_success_rate
     test_case = test_num
     success = 0
     for i in range(test_case):
 
-        flee_success = anim.did_flee_succeed()
+        flee_success = anim.is_flee_successful()
         # print(hunt_sucess)
         if (flee_success):
             success += 1
@@ -52,7 +57,19 @@ def test_did_flee_suceed(flee_success_rate, test_num=1000):
     print(f'Flee success rate: {success / test_case}')
 
 
-test_did_hunt_suceed(0.8, 1000)
-test_did_hunt_suceed(0.1, 1000)
-test_did_flee_suceed(0.3, 1000)
-test_did_flee_suceed(1, 1000)
+def test_has_starved(starvation_time):
+    anim = Animal()
+    anim.starvation_time = starvation_time
+    if anim.has_starved():
+        print('Animal starved to death')
+    else:
+        print('Animal is not starving')
+
+
+# test_is_hunt_successful(0.8, 1000)
+# test_is_hunt_successful(0.1, 1000)
+# test_is_flee_successful(0.3, 1000)
+# test_is_flee_successful(1, 1000)
+test_has_starved(0)
+test_has_starved(1)
+test_has_starved(-1)
