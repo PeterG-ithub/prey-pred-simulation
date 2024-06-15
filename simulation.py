ccc7443ac438ea900a68ee995a3eb4ba812b5f6a
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-15 10:50:35 -0700

    Add test for create_simulation function

--- simulation.py
+++ simulation.py
@@ -11,11 +11,23 @@ class Simulation:
         self.preys = []
         self.predators = []
 
-    def create_simulatoin(self):
-        for i in range(self.initial_prey_count):
+    def create_simulation(self):
+        for _ in range(self.initial_prey_count):
             anim = Animal('prey')
             self.preys.append(anim)
 
-        for i in range(self.initial_predator_count):
+        for _ in range(self.initial_predator_count):
             anim = Animal('predator')
             self.predators.append(anim)
+
+
+def test_create_simulation(prey_count, predator_count):
+    sim = Simulation()
+    sim.initial_predator_count = prey_count
+    sim.initial_prey_count = predator_count
+    sim.create_simulation()
+    print(f'Prey count: {len(sim.preys)}')
+    print(f'Predator count: {len(sim.predators)}')
+
+
+test_create_simulation(100, 10)
