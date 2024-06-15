a8844f1b1640ce87d52638afe4afe2ec931c7543
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-15 10:00:36 -0700

    Add animal class

---
+++ animal.py
@@ -0,0 +1,8 @@
+
+class Animal:
+    def __init__(self) -> None:
+        self.hunt_success_rate = 0.0
+        self.flee_success_rate = 0.0
+        self.birth_rate = 0.0
+        self.starvation_time = 0
+        
