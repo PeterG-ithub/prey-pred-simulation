453052a88adddd65894321c4d049ca51c9356f45
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-15 10:11:45 -0700

    Add function and test to Animal
    - Add roll function to roll from 0 to 1, float
    - Add did_hunt_succeed function to check base on hunt success, bool
    - Add test outside Animal to test if function working properly

--- animal.py
+++ animal.py
@@ -1,3 +1,5 @@
+import random
+
 
 class Animal:
     def __init__(self) -> None:
@@ -5,4 +7,30 @@ class Animal:
         self.flee_success_rate = 0.0
         self.birth_rate = 0.0
         self.starvation_time = 0
-        
+
+    def roll(self):
+        return random.random()
+
+    def did_hunt_succeed(self) -> bool:
+        if self.roll() < self.hunt_success_rate:
+            return True
+        return False
+
+
+def test_did_hunt_suceed(hunt_success_rate, test_num=1000):
+    anim = Animal()
+    anim.hunt_success_rate = hunt_success_rate
+    test_case = test_num
+    success = 0
+    for i in range(test_case):
+
+        hunt_sucess = anim.did_hunt_succeed()
+        # print(hunt_sucess)
+        if (hunt_sucess):
+            success += 1
+
+    print(f'Success rate: {success / test_case}')
+
+
+test_did_hunt_suceed(0.8, 1000)
+test_did_hunt_suceed(0.1, 1000)
