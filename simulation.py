2f1d1c1119faa73980ab8934eac16968af25f8fb
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-16 11:24:37 -0700

    Add attributes and function to Simulate
    - Add flee success rate attr and modified create_simulation with it
    - Add hunt success rate attr and modified create_simulation with it
    - Add starvation time attr and modified create_simulation with it
    - Add simulate_one_time_step function which has simulation of hunting
    successfulness for the predators
    - Add test for that function

--- simulation.py
+++ simulation.py
@@ -6,7 +6,10 @@ class Simulation:
         self.initial_prey_count = 0
         self.initial_predator_count = 0
         self.prey_birth_rate = 0.0
+        self.prey_flee_success_rate = 0.0
         self.predator_birth_rate = 0.0
+        self.predator_hunt_success_rate = 0.0
+        self.predator_starvation_time = 0
         self.create_simulation()
         self.preys = []
         self.predators = []
@@ -14,20 +17,55 @@ class Simulation:
     def create_simulation(self):
         for _ in range(self.initial_prey_count):
             anim = Animal('prey')
+            anim.flee_success_rate = self.prey_flee_success_rate
             self.preys.append(anim)
 
         for _ in range(self.initial_predator_count):
             anim = Animal('predator')
+            anim.hunt_success_rate = self.predator_hunt_success_rate
+            anim.starvation_time = self.predator_starvation_time
             self.predators.append(anim)
+
+    def simulate_one_time_step(self):
+        predator_index = 0
+        prey_index_list = []  # List of all the hunted preys
+        for prey_index in range(len(self.preys)):
+            if predator_index > len(self.predators) - 1:
+                predator_index = 0
+            if self.predators[predator_index].is_hunt_successful():
+                prey_index_list.append(prey_index)
+                self.predators[predator_index].starvation_time = 5
+            else:
+                self.predators[predator_index].starvation_time -= 1
+        
+        # Remove the hunted preys
+        prey_index_list.reverse()
+        for index in prey_index_list:
+            self.preys.pop(index)
 
 
 def test_create_simulation(prey_count, predator_count):
     sim = Simulation()
-    sim.initial_predator_count = prey_count
-    sim.initial_prey_count = predator_count
+    sim.initial_predator_count = predator_count
+    sim.initial_prey_count = prey_count
     sim.create_simulation()
     print(f'Prey count: {len(sim.preys)}')
     print(f'Predator count: {len(sim.predators)}')
 
 
-test_create_simulation(100, 10)
+def test_simulate_one_time_step(prey_count, predator_count):
+    sim = Simulation()
+    sim.initial_predator_count = predator_count
+    sim.initial_prey_count = prey_count
+    sim.predator_hunt_success_rate = 0.5
+    sim.predator_starvation_time = 5
+    sim.create_simulation()
+
+    print(f'Prey count before hunt: {len(sim.preys)}')
+    print(f'Starvation time before hunt of one of the predator: {sim.predators[0].starvation_time}')
+    sim.simulate_one_time_step()
+    print(f'Prey count after hunt: {len(sim.preys)}')
+    print(f'Starvation time after hunt of the same predator: {sim.predators[0].starvation_time}')
+
+# test_create_simulation(100, 10)
+test_simulate_one_time_step(100, 10)
